ff5aa60b8486fd537cba3a5213846f1d00b524f0
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-02-14 20:12:11 +0530

    cors integration

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const dbConnection = require('./databaseConnect')
 const ResponseRoute = require('./routes/response')
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/response', ResponseRoute);
